1d1257cc50c168d820daddaccb534cc01ad2442a
Maksym Vynohradov <mv@madappgang.com>
2020-04-22 12:56:01 +0300

    ci: update configuration

--- runCi.sh
+++ runCi.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export CYPRESS_BASE_URL=http://localhost:4000/
+export CYPRESS_BASE_URL=http://localhost:3000/simple-time-tracker
 
-yarn cypress run
+yarn cypress run --headless --browser chrome
