896c2871e887eca175d3ef3e655bd09230186dce
Maksym Vynohradov <mv@madappgang.com>
2020-04-22 16:03:29 +0300

    feat: add images to mochaavesome report

--- runCi.sh
+++ runCi.sh
@@ -2,4 +2,14 @@
 
 export CYPRESS_BASE_URL=http://localhost:3000/simple-time-tracker
 
+# clean-up report folder
+rm -R -f report && mkdir report
+
+# run tests
 yarn cypress run --headless --browser chrome
+
+# # combine report
+node mergeReports.js
+
+# # generate-report
+yarn marge ./report/mochawesome-report/report.json -f index -o report
